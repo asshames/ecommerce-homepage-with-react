ed123eed235770dabde3524ab72094fbb66c9e78
asshames <assamialomarshames@gmail.com>
2022-03-02 10:06:09 +0600

    adding news Component

--- ecommerce-homepage-with-react/ecom-home-page/src/containers/MyShop/MyShop.js
+++ ecommerce-homepage-with-react/ecom-home-page/src/containers/MyShop/MyShop.js
@@ -1,31 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import Layout from "../../components/Layout/Layout";
 import Aux from "../../hoc/Auxilary/Auxillary";
 import Slider from "../../components/Slider/Slider";
+import News from "../../components/News/News";
 
-class MyShop extends Component{
-    render() {
-        return (
-            <Aux>
-                <Layout />
-                <Slider />
-                <div>News</div>
-                <div>ShopMall, District, Campaigns, Services</div>
-                <div>Today's Deal</div>
-                <div>Winter Collections</div>
-                <div>Electronics</div>
-                <div>Women's Collection</div>
-                <div>Ladies Bag Collections</div>
-                <div>Global Products</div>
-                <div>Sharee's collection</div>
-                <div>SmartPhones</div>
-                <div>Flagship Store</div>
-                <div>Ekshop</div>
-                <div>Just For You</div>
-                <div>Flooter</div>
-            </Aux>
-        );
-    };
+const MyShop = () => {
+    return (
+        <Aux>
+            <Layout />
+            <Slider />
+            <News />
+            <div>ShopMall, District, Campaigns, Services</div>
+            <div>Today's Deal</div>
+            <div>Winter Collections</div>
+            <div>Electronics</div>
+            <div>Women's Collection</div>
+            <div>Ladies Bag Collections</div>
+            <div>Global Products</div>
+            <div>Sharee's collection</div>
+            <div>SmartPhones</div>
+            <div>Flagship Store</div>
+            <div>Ekshop</div>
+            <div>Just For You</div>
+            <div>Flooter</div>
+        </Aux>
+    );
 };
 
 export default MyShop;
